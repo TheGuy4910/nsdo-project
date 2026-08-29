f2b6068145349b03aaf5208f14eb2c86e6287af6
TheGuy4910 <olagunjuibrahim49@gmil.com>
2026-08-29 05:41:50 +0100

    Fix unclosed anchor tag in nav sign-in link

--- frontend/nsdo.js
+++ frontend/nsdo.js
@@ -219,9 +219,9 @@ function navHTML(activePage) {
         </a>`).join('')}
     </div>
     <div class="nav-status">
-  <a href="login.html" id="nav-signin-link" style="color:inherit;text-decoration:none;margin-right:14px;font-size:13px;">Sign in</a>
-  <span class="nav-status-dot" id="nav-status-dot"></span>
-  <span id="nav-status-text">Checking…</span>
+<a href="login.html" id="nav-signin-link" style="color:inherit;text-decoration:none;margin-right:14px;font-size:13px;">Sign in</a>
+<span class="nav-status-dot" id="nav-status-dot"></span>
+<span id="nav-status-text">Checking…</span>
 </div>
   </div>
 </nav>`;
